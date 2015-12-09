8b1769eb34e3b69710248df769add655b646fda3
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2015-12-09 16:14:35 +0000

    Update to use register data and latest form styles

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,38 @@
 module.exports = {
   bind : function (app) {
 
-    app.get('/', function (req, res) {
-      res.render('index');
-    });
+    var fs = require('fs');
 
-    app.get('/examples/template-data', function (req, res) {
-      res.render('examples/template-data', { 'name' : 'Foo' });
-    });
 
-    // add your routes here
+    var renderPage = function(params, callback){
+
+      var data = {};
+
+      // Load register from JSON file
+      data.country = JSON.parse(fs.readFileSync('app/data/country-records.json', 'utf8'));
+
+      // Sort register alphabetically by country name
+      data.country.sort(function(a, b) {
+        a = a.entry.name.toLowerCase();
+        b = b.entry.name.toLowerCase();
+
+        return a < b ? -1 : a > b ? 1 : 0;
+      })
+
+      return callback(null, data);
+
+    }
+
+
+
+    app.get("/", function (req, res, next) {
+      var params = {};
+
+      renderPage(params, function(error, data){
+        return res.render('index', data);
+      })
+
+    });
 
   }
 };
