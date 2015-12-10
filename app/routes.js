e3d3c17feb772eaaa79c9941183e93e2bb7aa1a5
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2015-12-10 16:48:17 +0000

    Add weightings and aliases for all countries

--- app/routes.js
+++ app/routes.js
@@ -6,10 +6,32 @@ module.exports = {
 
     var renderPage = function(params, callback){
 
-      var data = {};
-
       // Load register from JSON file
+      var data = {};
       data.country = JSON.parse(fs.readFileSync('app/data/country-records.json', 'utf8'));
+
+      // Load additional country data from JSON file
+      var extension = {};
+      extension = JSON.parse(fs.readFileSync('app/data/country-records-extension.json', 'utf8'));
+
+      for(var i = 0; i < data.country.length; i++){
+
+        var entry = data.country[i].entry;
+        var country = entry.country
+
+        for(var j = 0; j < extension.length; j++){
+
+          if (country == extension[j].country){
+            data.country[i].entry.aliases = extension[j].aliases;  
+            data.country[i].entry.weighting = extension[j].weighting;
+          }
+
+        } 
+
+      }
+
+      //console.log(JSON.stringify(data.country));
+
 
       // Sort register alphabetically by country name
       data.country.sort(function(a, b) {
@@ -30,6 +52,15 @@ module.exports = {
 
       renderPage(params, function(error, data){
         return res.render('index', data);
+      })
+
+    });
+
+    app.get("/register", function (req, res, next) {
+      var params = {};
+
+      renderPage(params, function(error, data){
+        return res.render('register', data);
       })
 
     });
